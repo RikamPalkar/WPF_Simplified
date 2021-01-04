c358fccd517fc51b9af4aaf94057b16c87383fe2
Rikam <36474843+RikamCZ@users.noreply.github.com>
2021-01-04 21:54:19 +0530

    Chapter 2

---
+++ Chapter 2/Listing 17/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWPFSample
+{
+    class Test
+    {
+        string simpleCookie = "CSCUser1=DonJoe";
+        string cookieWithExpiration = "CSCUser2=DonJoe;expires=Mon, 1-Mar-2021 00:00:00 GMT";
+
+        Uri simpleCookieUri = new Uri(@"C:\Junk\SimpleCookie");
+        Uri persistenetCookieUri = new Uri(@"C:\Junk\PersistentCookie");
+
+        Application.SetCookie(simpleCookieUri, simpleCookie);
+        Application.SetCookie(persistenetCookieUri, cookieWithExpiration);
+
+    }
+}
---
+++ Chapter 2/Listing 27/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWPFSample
+{
+    class Test
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Get CommandLine Arguments
+            bool fullScreen = false;
+            foreach (string arg in e.Args)
+            {
+                if (arg == "")
+                    fullScreen = true;
+            }
+
+            // Set Application level data properties
+            this.Properties["Login"] = "DonJoe";
+            this.Properties["Password"] = "pwd";
+
+            // Create a Window, set its properties and display it
+            SecondWindow window = new SecondWindow();
+            if (fullScreen)
+            {
+                window.WindowState = WindowState.Minimized;
+            }
+            window.Show();
+        }
+    }
+}
