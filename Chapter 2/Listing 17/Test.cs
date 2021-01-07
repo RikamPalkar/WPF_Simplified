31710291ef2dbf6c1296cd615e9ddd3a971cc0ac
Vinoth Rajendran <r.vinoth@live.com>
2021-01-07 20:33:26 +0100

    updated

--- Chapter 2/Listing 17/Test.cs
+++ Chapter 2/Listing 17/Test.cs
@@ -6,14 +6,14 @@ namespace HelloWPFSample
 {
     public class SaveCookie
     {
-        string simpleCookie = "CSCUser1=DonJoe";
-        string cookieWithExpiration = "CSCUser2=DonJoe;expires=Mon, 1-Mar-2021 00:00:00 GMT";
+        const string simpleCookie = "CSCUser1=DonJoe";
+        const string cookieWithExpiration = "CSCUser2=DonJoe;expires=Mon, 1-Mar-2021 00:00:00 GMT";
 
-        Uri simpleCookieUri = new Uri(@"C:\Junk\SimpleCookie");
-        Uri persistenetCookieUri = new Uri(@"C:\Junk\PersistentCookie");
+        var simpleCookieUri = new Uri(@"C:\Junk\SimpleCookie");
+        var persistentCookieUri = new Uri(@"C:\Junk\PersistentCookie");
 
         Application.SetCookie(simpleCookieUri, simpleCookie);
-        Application.SetCookie(persistenetCookieUri, cookieWithExpiration);
+        Application.SetCookie(persistentCookieUri, cookieWithExpiration);
 
     }
 }
