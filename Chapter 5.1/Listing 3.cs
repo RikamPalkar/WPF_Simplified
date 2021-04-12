0540a1bb63304b0a7224fc8e5e5556229724f157
Rikam <36474843+RikamCZ@users.noreply.github.com>
2021-04-12 16:41:10 +0530

    Commiting Listings

---
+++ Chapter 5.1/Listing 3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Hierarchy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void AddResourcesToApplication()
+        {
+            Application curApp = Application.Current;
+            curApp.Resources.Add("YellowSolidBrush", new SolidColorBrush(Colors.Yellow));
+            curApp.Resources.Add("GrayBorderBrush", new SolidColorBrush(Colors.Gray));
+        }
+
+
+
+    }
+}
---
+++ Chapter 5.1/Listing 9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Hierarchy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // Add Resources to a Window
+        this.Resources.Add("YellowSolidBrush", new SolidColorBrush(Colors.Yellow));
+        this.Resources.Add("GrayBorderBrush", new SolidColorBrush(Colors.Gray));
+        // Get and Set resources 
+        button1.Background = (Brush) this.FindResource("YellowSolidBrush");
+        button1.BorderBrush = (Brush) this.FindResource("GrayBorderBrush");
+
+
+        }
+    }
