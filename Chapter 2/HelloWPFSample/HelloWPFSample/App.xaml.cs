ea9fc4b02fbb5a5fb373ff73e819b7030dd51878
Rikam <rikampalkar@gmail.com>
2021-04-19 19:49:15 +0530

    Checking in Solution 1
    Listing 1 - 9

---
+++ Chapter 2/HelloWPFSample/HelloWPFSample/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWPFSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region ApplicationLevelProperties
+        string uid = Application.Current.Properties["Login"].ToString();
+        string pwd = Application.Current.Properties["Password"].ToString();
+        #endregion
+
+        /// <summary>
+        /// App_Startup
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            this.Properties["Login"] = "Peter";
+            this.Properties["Password"] = "12345";
+
+            // Create and store an object in Properties
+            Author author = new Author("Peter", 33, "Programming");
+            this.Properties["CurrentAuthor"] = author;
+
+        }
+
+    }
+}
---
+++ Chapter 2/HelloWPFSample/HelloWPFSample/Author.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWPFSample
+{
+    /// <summary>
+    /// The Author Classs
+    /// </summary>
+    public class Author
+    {
+        /// <summary>
+        /// Constructor of Author
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        /// <param name="bookName"></param>
+        public Author(string name, Int16 age, string bookName)
+        {
+            this.AuthorName = name;
+            this.AuthorAge = age;
+            this.PublishedBook = bookName;
+        }
+
+        /// <summary>
+        /// The property name
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// The property AuthorName
+        /// </summary>
+        public string AuthorName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// The property age
+        /// </summary>
+        private Int16 age;
+        /// <summary>
+        /// The property AuthorAge
+        /// </summary>
+        /// 
+        public Int16 AuthorAge
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        /// <summary>
+        /// The property book
+        /// </summary>
+        private string book;
+
+        /// <summary>
+        /// The property PublishedBook
+        /// </summary>
+        public string PublishedBook
+        {
+            get { return book; }
+            set { book = value; }
+        }
+    }
+}
