7336053bb271d27a3aa795fd56534c31135bf738
Rikam <rikampalkar@gmail.com>
2021-04-19 21:12:32 +0530

    Chapter 2 Source code check-in

---
+++ Chapter 2/Listing 9 - 45/HelloWPFSample/HelloWPFSample/App.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Resources;
+
+namespace HelloWPFSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Listing 21
+            Application app = new Application();
+            app.Run();
+
+
+            //Listing 22
+            Application app1 = new Application();
+            app.Run(new MainWindow());
+
+        }
+
+        /// <summary>
+        /// Listing 24, 25, 45
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Get CommandLine Arguments
+            bool fullScreen = false;
+            foreach (string arg in e.Args)
+            {
+                if (arg == "")
+                    fullScreen = true;
+            }
+
+            // Set Application level data properties
+            this.Properties["Login"] = "DonJoe";
+            this.Properties["Password"] = "pwd";
+
+            // Create a Window, set its properties and display it
+            //splash screen Listing 44
+            SecondWindow window = new SecondWindow();
+            window.Top = 100;
+            window.Left = 400;
+            window.Show();
+
+            if (fullScreen)
+            {
+                window.WindowState = WindowState.Minimized;
+            }
+            window.Show();
+
+            //Getting Command Line Arguments 
+            // CommandLine Arguments found?
+            if (e.Args.Length > 0)
+            {
+                // Loop through all command-line arguments
+                // which is an array of strings
+                foreach (string arg in e.Args)
+                {
+                    MessageBox.Show("Arg =" + arg);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Listing 27, 28
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Activated(object sender, EventArgs e)
+        {
+            MessageBox.Show("Activated event fired.");
+        }
+
+        /// <summary>
+        /// Listing 27, 28
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Deactivated(object sender, EventArgs e)
+        {
+            MessageBox.Show("Deactivated event fired.");
+        }
+
+
+        /// <summary>
+        /// Listing 30, 31
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_SessionEnding(object sender, SessionEndingCancelEventArgs e)
+        {
+            string msg = string.Format("{0}. Do you want to close this session?", e.ReasonSessionEnding);
+            MessageBoxResult result = MessageBox.Show(msg, "Session Ending Event Fired.", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// Listing 33
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Listing 35, 36
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            // Do something about the exception
+            MessageBox.Show("Exception occured while dividing by zero!");
+            e.Handled = true;
+        }
+    }
+}
---
+++ Chapter 2/Listing 9 - 45/HelloWPFSample/HelloWPFSample/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Resources;
+using System.Windows.Shapes;
+
+namespace HelloWPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Listing 38
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //initialize window specific resources here.
+        }
+
+        /// <summary>
+        /// Listing 38
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            //release window specific resources here.
+        }
+
+        /// <summary>
+        /// Listing 11
+        /// </summary>
+        private void FindResourceMethod()
+        {
+            try
+            {
+                object res = Application.Current.FindResource("YellowSolidBrush");
+                SolidColorBrush yellowBrush = (SolidColorBrush)res;
+            }
+            catch (ResourceReferenceKeyNotFoundException resExp)
+            {
+                MessageBox.Show("Resource not found." + resExp.Message);
+            }
+        }
+
+        /// <summary>
+        /// Listing 12
+        /// </summary>
+        private void TryFindResourceMethod()
+        {
+            object res1 = Application.Current.TryFindResource("YellowSolidBrush");
+            if (res1 != null)
+            {
+                SolidColorBrush yellowBrush = (SolidColorBrush)res1;
+            }
+        }
+
+        /// <summary>
+        /// Listing 14, Listing 15
+        /// </summary>
+        private void ShutdownModeMethod()
+        {
+            Application curApp = Application.Current;
+            curApp.ShutdownMode = ShutdownMode.OnLastWindowClose;
+
+            Application curApp1 = Application.Current;
+            curApp1.Shutdown();
+        }
+
+        /// <summary>
+        /// Listing 16
+        /// </summary>
+        private void StartupMethod()
+        {
+            Application curApp = Application.Current;
+            curApp.StartupUri = new Uri("SecondWindow.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        /// <summary>
+        /// Listing 18, Listing 19
+        /// </summary>
+        private void ManagingCookies()
+        {
+            string simpleCookie = "CSCUser1=DonJoe";
+            string cookieWithExpiration = "CSCUser2=DonJoe;expires=Mon, 1-Mar-2021 00:00:00 GMT";
+
+            Uri simpleCookieUri = new Uri(@"C:\Junk\SimpleCookie");
+            Uri persistenetCookieUri = new Uri(@"C:\Junk\PersistentCookie");
+
+            Application.SetCookie(simpleCookieUri, simpleCookie);
+            Application.SetCookie(persistenetCookieUri, cookieWithExpiration);
+
+            //Application.GetCookie
+            Uri cookiePath = new Uri(@"C:\Junk\SimpleCookie");
+            string cookie = Application.GetCookie(cookiePath);
+            MessageBox.Show(cookie);
+        }
+
+        /// <summary>
+        /// Listing 39
+        /// </summary>
+        private void GetResourceStream()
+        {
+            Uri uri = new Uri("/ResourceFile.xaml", UriKind.Relative);
+            StreamResourceInfo info = Application.GetResourceStream(uri);
+        }
+
+        /// <summary>
+        /// Listing 40
+        /// </summary>
+        private void GetContentStream()
+        {
+            Uri uri = new Uri("/ContentFile.xaml", UriKind.Relative);
+            StreamResourceInfo info = Application.GetContentStream(uri);
+        }
+
+        /// <summary>
+        /// Listing 41
+        /// </summary>
+        private void GetRemoteStream()
+        {
+            Uri uri = new Uri("/SOOFile.xaml", UriKind.Relative);
+            StreamResourceInfo info = Application.GetRemoteStream(uri);
+        }
+
+        private void LoadComponent()
+        {
+            object comp = Application.LoadComponent(
+            new Uri("C:/MyProject/SecondWindow.xaml", System.UriKind.RelativeOrAbsolute));
+        }
+    }
+}
