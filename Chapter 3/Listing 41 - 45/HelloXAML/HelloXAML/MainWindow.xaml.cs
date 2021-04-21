a7abcfb08483460cda166a06c6aba81d3a42719d
Rikam <rikampalkar@gmail.com>
2021-04-21 16:53:59 +0530

    Chapter 3 - Project 7
    Listing 41 - 45

---
+++ Chapter 3/Listing 41 - 45/HelloXAML/HelloXAML/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloXAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxBeverages.Items.Add(TextBoxUserInput.Text);
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxBeverages.Items.RemoveAt
+                (ListBoxBeverages.Items.IndexOf(ListBoxBeverages.SelectedItem));
+        }
+
+
+    }
+}
