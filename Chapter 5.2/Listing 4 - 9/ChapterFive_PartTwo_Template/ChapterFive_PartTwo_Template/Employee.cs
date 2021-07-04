fc94442d996404cf0dbb8f14b783218b4bc6e87a
Rikam <rikampalkar@gmail.com>
2021-07-04 16:24:32 +0530

    Chapter 5 - Project 2
    Listing 4 - 9

---
+++ Chapter 5.2/Listing 4 - 9/ChapterFive_PartTwo_Template/ChapterFive_PartTwo_Template/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ChapterFive_PartTwo_Template
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Designation { get; set; }
+    }
+}
---
+++ Chapter 5.2/Listing 4 - 9/ChapterFive_PartTwo_Template/ChapterFive_PartTwo_Template/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace ChapterFive_PartTwo_Template
+{
+    public class MainWindowViewModel
+    {
+
+        #region Properties 
+        private Employee _empDetails;
+        public Employee EmpDetails
+        {
+            get { return _empDetails; }
+            set { _empDetails = value; }
+        }
+
+        #endregion
+
+        #region Constructor 
+        public MainWindowViewModel()
+        {
+            EmpDetails = new Employee()
+            {
+                EmpId = 1,
+                EmpName = "Alex",
+                Designation = "Software Engineer"
+            };
+        }
+        #endregion
+    }
+}
